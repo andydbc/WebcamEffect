e7e9d605c4a30b553dad5456383027785a94e056
Andy Duboc <a@andydbc.xyz>
2020-07-04 06:01:19 -0400

    Initial Commit

---
+++ Assets/WebcamCapture.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class WebcamCapture : MonoBehaviour
+{
+    /// <summary>
+    /// Material used to play with the webcam texture.
+    /// </summary>
+    [SerializeField]
+    private Material _material = default;
+
+    /// <summary>
+    /// Texture containing the webcam capture, will be send to the shader.
+    /// </summary>
+    private WebCamTexture _webCamTexture;
+
+    /// <summary>
+    /// Initialize the webcam texture and start capturing.
+    /// </summary>
+    private void Start()
+    {
+        if (_webCamTexture == null)
+        {
+            _webCamTexture = new WebCamTexture();
+        }
+
+        _webCamTexture.requestedHeight = 512;
+        _webCamTexture.requestedWidth = 512;
+        _webCamTexture.Play();
+    }
+
+    /// <summary>
+    /// Send texture to the shader.
+    /// </summary>
+    private void Update()
+    {
+        if (_material == null)
+            return;
+
+        _material.SetTexture("_WebcamTex", _webCamTexture);
+    }
+}
